7f611b3a27645d300c3a1a99659097614046026f
Marc Burns <m4burns@uwaterloo.ca>
2018-01-06 17:47:30 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'require-py',
+  packages = ['require-py'],
+  version = '0.1',
+  description = 'my preferred module semantics for python',
+  author = 'Marc Burns',
+  author_email = 'm4burns@uwaterloo.ca',
+  url = 'https://github.com/m4burns/require',
+  download_url = 'https://github.com/m4burns/require/archive/0.1.tar.gz',
+  keywords = ['module', 'package', 'import', 'require'],
+  classifiers = [],
+)
